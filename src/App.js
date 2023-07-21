15e48e0d92c865c6dca08c0d242a2210217efff1
JACKS9969 <101617802+JACKS9969@users.noreply.github.com>
2023-07-21 11:37:31 +0530

    final two

--- src/App.js
+++ src/App.js
@@ -21,30 +21,8 @@ import Home from './components/Home';
 
 
 function App() {
-  const[mode, setMode] = useState('light');
-  const [alert, setAlert] = useState(null);
-
-  const showAlert = (message, type)=>{
-    setAlert({
-      msg: message,
-      type: type
-    })
-    setTimeout(() => {
-      setAlert(null);
-    }, 1500);
-  }
-  const toggleMode = ()=>{
-    if(mode ==='light'){
-      setMode('dark');
-      document.body.style.backgroundColor ='black';
-      showAlert("Dark mode has been enabled", "success");
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor ='white';
-      showAlert("Light mode has been enabled", "success");
-    }
-  }
+  
+ 
   return (
     <>
     <BrowserRouter>
