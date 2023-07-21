dd248b2c2c85f584fcac374c2b7ed03bfd71a29a
JACKS9969 <101617802+JACKS9969@users.noreply.github.com>
2023-07-21 11:40:02 +0530

    Updates

---
+++ precache-manifest.4f36e62abcd1ac7bb2ea9c66e06aa6b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccfe4e85bd08febe7b35",
+    "url": "/my-app/static/css/main.0584b815.chunk.css"
+  },
+  {
+    "revision": "ccfe4e85bd08febe7b35",
+    "url": "/my-app/static/js/main.ccfe4e85.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/my-app/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "b59ec351bd7b7a60f691",
+    "url": "/my-app/static/js/2.b59ec351.chunk.js"
+  },
+  {
+    "revision": "fe08e4d762982826d1b1",
+    "url": "/my-app/static/js/runtime~main.fe08e4d7.js"
+  },
+  {
+    "revision": "dd4c96e16ad77777268dad12c73ad9d0",
+    "url": "/my-app/index.html"
+  }
+];
